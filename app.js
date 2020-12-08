2c6be6f263c5f792816b2820b5a3ffcfd259d2c1
Valeria Velasquez <39226355+valeriavelasquez@users.noreply.github.com>
2020-12-08 01:03:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express=require("express");
 var bodyParser=require("body-parser"); 
   
 const mongoose = require('mongoose'); 
-mongoose.connect('mongodb+srv://valeriavelasquez:tomato@cluster0.bmsdw.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true }); 
+mongoose.connect('mongodb+srv://valeriavelasquez:tomato@cluster0.bmsdw.mongodb.net/forApp?retryWrites=true&w=majority', { useNewUrlParser: true }); 
 var db=mongoose.connection; 
 db.on('error', console.log.bind(console, "connection error")); 
 db.once('open', function(callback){ 
@@ -23,27 +23,49 @@ app.post('/signUp', function(req,res){
     var fname = req.body.fname; 
     var lname =req.body.lname; 
     var email = req.body.email; 
-    var age =req.body.age; 
-  
+    var age =req.body.age;
+    var q1 = req.body.q1;
+    var q2 = req.body.q2;
+    var q3 = req.body.q3;
+    var q4 = req.body.q4;
+    var q5 = req.body.q5;
+    var q6 = req.body.q6;
+    var q7 = req.body.q7;
+    var q8 = req.body.q8;
+    var q9 = req.body.q9;
+    var q10 = req.body.q10;
+    var q11= req.body. q11;
+    var q12 = req.body.q12 ;
+
     var data = { 
         "fname": fname, 
         "lname":lname, 
         "email": email, 
-        "age": age 
+        "age": age,
+        "q1": q1,
+        "q2": q2,
+        "q3": q3,
+        "q4": q4, 
+        "q5": q5,
+        "q6": q6, 
+        "q7": q7,
+        "q8": q8, 
+        "q9": q9,
+        "q10": q10,
+        "q11": q11,
+        "q12": q12   
+        
     } 
 db.collection('toMatch').insertOne(data,function(err, collection){ 
         if (err) throw err; 
         console.log("Record inserted Successfully"); 
               
     }); 
-          
-    return res.redirect('index.html'); 
 }) 
 
-app.get('/signUp',function(req,res){ 
-    res.render("index");
-}).listen(8080) 
+app.get('/',function(req,res){ 
+    res.sendFile(__dirname + "/index.html");
+}).listen(3000) 
   
   
-console.log("server listening at port 8080"); 
-
+console.log("server listening at port 3000"); 
